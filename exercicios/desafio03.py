7a67193445cee1cefad0cdfd3c05b1a06cad6e82
Pedro <98357661+wantedi@users.noreply.github.com>
2022-01-25 16:55:31 -0300

    Fixing Bugs

--- exercicios/desafio03.py
+++ exercicios/desafio03.py
@@ -1,3 +1,4 @@
-num1 = input('Primeiro número')
-num2 = input('Segundo numéro')
-print('A soma é', num1 + num2)
+num1 = int(input('Primeiro número'))
+num2 = int(input('Segundo numéro'))
+r = num1 + num2
+print(f'A soma de {num1} e {num2} vale {r}')
