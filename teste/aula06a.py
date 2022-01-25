e1ffdc264bc999afb5f7aef5cba6273f6b2c9079
Pedro <98357661+wantedi@users.noreply.github.com>
2022-01-25 17:18:33 -0300

    Atualização dos Exercícios

---
+++ teste/aula06a.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+r = n1 + n2
+# print('A soma entre', n1, 'e', n2, 'vale {}'.format(n1 + n2))
+print(f'A soma entre {n1} e {n2} vale {r}')
