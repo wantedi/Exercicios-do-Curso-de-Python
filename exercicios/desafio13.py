793e6b1973725ef484917cf6d6d5f8f2bc05852e
Pedro <98357661+wantedi@users.noreply.github.com>
2022-02-21 21:27:05 -0300

    Create desafio13.py

---
+++ exercicios/desafio13.py
@@ -0,0 +1,4 @@
+s = float(input('Digite o valor do seu sálario:R$'))
+a = float(input('Digite o valor do aumento:'))
+r = s*a/100
+print(f'O seu sálario que era {s} com o aumento de {a}% agora é:{s+r:.2}R$')
