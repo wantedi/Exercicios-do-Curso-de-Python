905ffa09e5651f131f10e711d3111fe39cdf13b4
Pedro <98357661+wantedi@users.noreply.github.com>
2022-02-22 19:59:49 -0300

    Create desafio016.py

---
+++ exercicios/desafio016.py
@@ -0,0 +1,6 @@
+'''from math import floor
+num = float(input('Digite um número real: '))
+print(f'A porção inteira de {num} é: ', floor(num))'''
+
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é:', int(num))
