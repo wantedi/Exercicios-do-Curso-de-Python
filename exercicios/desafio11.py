196bce1bea01b7b13d2f07defaa51e8441cc9633
Pedro <98357661+wantedi@users.noreply.github.com>
2022-02-21 21:25:28 -0300

    Create desafio11.py

---
+++ exercicios/desafio11.py
@@ -0,0 +1,4 @@
+a = float(input('Digite a altura da sua parede:'))
+la = float(input('Digite a largura da sua parede:'))
+print(f'Sua parede tem a dimensão de {a}x{la} e sua área é de {a*la}m² quadrados')
+print(f'Você precisa de {(a*la)/2}L de tinta para pintar a parede.')
