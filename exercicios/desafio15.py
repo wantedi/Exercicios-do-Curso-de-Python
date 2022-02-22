89a9f5e0a019dec5b3761782600a2e830280dbb4
Pedro <98357661+wantedi@users.noreply.github.com>
2022-02-21 21:43:29 -0300

    Create desafio15.py

---
+++ exercicios/desafio15.py
@@ -0,0 +1,4 @@
+dia = int(input('Por quantos dias o carro foi alugado? '))
+km = float(input('Quantos quilômetros foram rodados? '))
+r = dia * 60 + km * 0.15
+print(f'O total a se pagar é de R${r}')
