4832d80b4587d3c8986c99cbf3ab16621503f9f3
Pedro <98357661+wantedi@users.noreply.github.com>
2022-03-03 17:45:01 -0300

    Create aula07a.py

---
+++ teste/aula07a.py
@@ -0,0 +1,13 @@
+n1 = int(input('Digite um valor:'))
+n2 = int(input('Digite outro valor:'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+# print(f'A soma é {s}', end=' ')
+# print(f' o produto é {m}', end=' ')
+# print (f'a divisão é {d:.3f}', end=' ')
+# print (f'a divisão inteira é {di}', end=' ')
+# print (f'e a potência é {e}', end=' ')
+print(f'A soma é {s}, \n o produto é {m}, \n a divisão é {d}, \n a divisão inteira é {di}, \n a potência é {e}')
