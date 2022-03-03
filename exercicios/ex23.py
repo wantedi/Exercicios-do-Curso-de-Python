ba2820ed67b18f160bf78858dd63119bc59f7d55
Pedro <98357661+wantedi@users.noreply.github.com>
2022-03-03 17:43:04 -0300

    Create ex23.py

---
+++ exercicios/ex23.py
@@ -0,0 +1,16 @@
+# num = input('Digite um número qualquer de 0 a 9999: ')
+# print(f'Unidade: {num[3]}')
+# print(f'Dezena: {num[2]}')
+# print(f'Centena: {num[1]}')
+# print(f'Milhar: {num[0]}')
+
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Analisando o número {num}')
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
